add81c06a49f6cc390d4f8c0d2859c1e9a444570
Stolati <stolati@gmail.com>
2012-01-08 19:41:43 +0100

    added simpler configuration

--- conf/profile.bash
+++ conf/profile.bash
@@ -3,9 +3,16 @@
 #configuration
 ACTIVE_HISTORY=${ACTIVE_HISTORY:-false} #true
 
-
 export et="${et:-`pwd`}" #home path (E.T. phone home)
 export bin="$et/env/bin"
+export conf="$et/env/conf"
+
+###########################
+# often used functions
+###########################
+str_lower(){ echo "$*" | tr '[:upper:]' '[:lower:]' ; }
+str_upper(){ echo "$*" | tr '[:lower:]' '[:upper:]' ; }
+pass(){ echo > /dev/null ; } #do nothing
 
 ###########################
 # Bash environment
@@ -15,7 +22,8 @@ chmod a+x $bin/* #just to be sure
 export PATH=".:$PATH:$bin"
 export CDPATH="${CDPATH:-}:$et"
 
-export SHELL="$bin/shell" #shell here is 
+export SHELL="$bin/shell" #shell here is
+export SCREEN="$bin/screen"
 
 set show-all-if-ambiguous on #show completion instead of bell
 set horizontal-scroll-mode on #dont wrap when line is too long
@@ -74,12 +82,12 @@ git config --global user.name "Stolati"
 git config --global user.email "stolati@gmail.com"
 
 ###########################
-# VIM commands
+# VIM settings
 ###########################
 
 #-i /dev/null (instead of viminfo) is where vim put its informations (so when we open a file again, some place are kepts)
 #-n is for no swap file
-EDITOR="vim -u $et/env/conf/vimrc -i /dev/null -n"
+EDITOR="$bin/editor" #because of spaces
 
 #bash mode vi
 set -o vi
@@ -92,7 +100,7 @@ bind -m vi-insert "\C-l":clear-screen # ^l clear screen
 
 
 ###########################
-# Aliases
+# Short smart aliases
 ###########################
 
 #the principle is to have a maximum of 1 char aliases
@@ -116,7 +124,7 @@ bind -m vi-insert "\C-l":clear-screen # ^l clear screen
 #l= ls -lrth
 #m=
 #n=
-#o=
+#o= open the suff it gives dir => nautilus/explorer, pdf => acrobat, etc ... host dependant
 #p=
 #q= exit
 #r= rm
@@ -186,7 +194,14 @@ g(){ #[-v] <pattern> [<files>]
 alias l='$LS'
 #alias m=
 #alias n=
-#alias o=
+o(){
+  case "$env_type" in
+    win) explorer "$@" ;;
+    lux) nautilus "$@" ;;
+    mac) open "$@" ;;
+    *) echo "Your environment is not defined";;
+  esac
+}
 alias p="profile_utils_dirqueue" #push a
 alias q=exit
 #r(){ echo "$*" | bc ;}
@@ -209,11 +224,9 @@ lc(){ ls "$@" | wc -l ; } #count number of files
 #TODO why it bug : lt(){ l | tail $1 ; } #l with tail
 llt(){ ll | tail $1 ; } #ll with tail
 
-gg(){ g $g "$@" ; } #use g variable for searching
 fg(){ find . -type f -print0 | xargs -0 egrep --color -i "$1" ; }
 
 lg(){ l | g "$@" ; }
-lgg(){ l | gg "$@" ; }
 
 alias pl='dirs -p -l'
 
@@ -225,23 +238,8 @@ TODO(){ find . -type f | xargs -n 100 grep -n TODO ; }
 alias eof='touchext .eof'
 alias EOF='touchext _EOF'
 
-#TODO -(){ return to the previous path }
-#TODO ?(){ calcul ;}
-#TODO mix cd and e into e
-
-
-#an egrep, case if contain MAJ, else no case
-#profile_smartGrep(){ #<patt> <files>
-#
-#
-#
-#
-#   egrep --color 
-#}
-
-
-
-
+-(){ c - ; }
+?(){ echo "$*" | bc -l ; }
 
 ###########################
 # change dir
@@ -249,6 +247,7 @@ alias EOF='touchext _EOF'
 
 #!/usr/bin/env bash
 
+#test a command for parameters, if we can use them or not
 gen_cmd(){ #<command> <last command values> <parameters>
   typeset cmd="$1" param="$2"
   shift; shift
@@ -260,32 +259,6 @@ gen_cmd(){ #<command> <last command values> <parameters>
 }
 
 LS=$(gen_cmd ls / -l -F -G --color=auto --group-directories-first)
-
-
-#TODO try to push the directory into the dirqueue
-#TODO and then have c and - with more than 2 values
-#try to push the directory into the dirqueue
-#if it's already the last of the queue, switch with the previous one
-# if there is an arg, try to go to the latest which contain the word
-#profile_utils_dirqueue(){
-  #typeset args="$*"
-
-  #if [ -n "$args" ];then
-
-  #fi
-
-  #set -x
-  #typeset previous="`dirs +1 -l `"
-  #typeset current="`pwd`"
-  #if [ "$current" = "$previous" ]; then
-  #  popd
-  #  popd
-  #  pwd
-  #  pushd "$current"
-  #else
-  #  pushd .
-  #fi
-#}
 
 #alias up to 10 level
 #so i don't have to count when moving
@@ -300,51 +273,10 @@ alias .........='c ../../../../../../../..'
 alias ..........='c ../../../../../../../../..'
 alias ...........='c ../../../../../../../../../..'
 
-
-
 ###########################
-# Editor
+# Bigger aliases for less used commands
 ###########################
 
-#load the editors values (vim)
-
-#the default editor
-EDITOR="vim -u $et/env/conf/vimrc -i $et/env/conf/viminfo -n -X"
-
-#bash mode vi
-set -o vi
-set editing-mode vi
-set convert-meta on
-
-bind -m vi-insert "\C-p":dynamic-complete-history # ^p check for partial match in history
-bind -m vi-insert "\C-n":menu-complete # ^n cycle through the list of partial matches
-bind -m vi-insert "\C-l":clear-screen # ^l clear screen
-
-
-#edit programm in the path
-profile_editor_editInPath(){
-  typeset path_prog=`which $1`
-  if [ ! -f "$path_prog" ];then
-    echo "No program found for [$1]"
-    return
-  fi
-  if [ ! -r "$path_prog" ];then
-    echo "No reading permission for [$path_prog]"
-    return
-  fi
-
-  $EDITOR "$path_prog"
-}
-
-
-
-
-
-#####################
-#old settings
-#####################
-
-#for humains
 alias df='df -h'  #sizes in humain format
 alias du='du -h'  #sizes in humain format
 alias less='less -r'       # raw control characters
@@ -353,49 +285,20 @@ alias wget='wget --no-check-certificate'
 alias man='man -a'
 alias diff='diff -r'
 
-export SCREEN="screen -c $et/env/conf/screenrc -a"
-
-#shorten most used commands
-#?(){ echo "$*" | bc -l ; }
-#alias p="project --conf-file=$et/env/conf/project"
-
-alias ss='serv -c $et/env/conf/hosts.lst -s' #connect through ssh
-alias sl='serv -c $et/env/conf/hosts.lst -l' #list all awailable servers
-alias sf='serv -c $et/env/conf/hosts.lst -f' #connect through ftp
-
-#TODO
-
-#don't take the already opened screen sessions
-#alias scre='screen -a -D -R'
-
-#bash variable sheet
-# ${toto#reg} delete the shortest match from the left
-# ${toto##reg} delete the longest match from the left
-# ${toto%reg} delete the shortest from the right
-# ${toto%%reg} delete the longest from the right
-# ${toto:-lala} $toto or "lala"
-# ${toto:=lala} $toto or toto="lala" ; $toto
-# ${toto:+lala} $toto and "lala" or ""
-# ${toto:?"lala"} $toto or echo "lala"
+alias ss='serv -c $conf/hosts.lst -s' #connect through ssh
+alias sl='serv -c $conf/hosts.lst -l' #list all awailable servers
+alias sf='serv -c $conf/hosts.lst -f' #connect through ftp
 
 #TODO secure actions
+#TODO http://stackoverflow.com/questions/7713021/recycle-bin-script-in-unix
 #alias mv='trash --conf-file=$et/conf/trash --simulate-mv'
 #alias cp='trash --conf-file=$et/conf/trash --simulate-cp'
 #alias rm='trash --conf-file=$et/conf/trash --simulate-rm'
+# it should use the stuff from operating system first
 
 #####################
 # Utils function
 #####################
-
-profile_utils_getColorFromEnv(){
-  case "$1" in
-    reset) echo "\e[0m";; #reset color
-    prod) echo "\e[1;31m";; #red
-    re7) echo "\e[1;32m";; #green
-    dev) echo "\e[1;34m";; #blue
-    *) echo "\e[1;37m";; #white
-  esac
-}
 
 #create a custom watch so i can use my aliases
 #Ctrl-c to quit
@@ -415,7 +318,7 @@ profile_utils_watch(){
 
 
 #configuration for pentadactyl
-export PENTADACTYL_INIT=":source \"$et/env/conf/pentadactyl.conf\""
+export PENTADACTYL_INIT=":source \"$conf/pentadactyl.conf\""
 
 #__EOF__
 
@@ -425,14 +328,13 @@ export PENTADACTYL_INIT=":source \"$et/env/conf/pentadactyl.conf\""
 #####################
 
 case "$OSTYPE" in
-  *cygwin*)  env="win" ;;
-  *linux*) env=lux ;;
-  *darwin*) env=mac ;;
-  *) env="what" ;;
+  *cygwin*) export env_type=win  ;;
+  *linux*)  export env_type=lux  ;;
+  *darwin*) export env_type=mac  ;;
+  *)        export env_type=what ;;
 esac
 
-base_app="$et/app/${env}_app"
-
+base_app="$et/app/${env_type}_app"
 
 #####################
 # Alias for executables
@@ -463,58 +365,40 @@ alias ff='nohup "$ff_path" -profile "$ff_profile" >/dev/null 2>&1 &'
 #####################
 # Load the profile bash
 #####################
+email="$(str_lower `whoami`@`hostname`)"
 
+source "$conf/local.conf"
 
-#!/usr/bin/env bash
+case "${TYPE_ENV:-}" in
+  dev) PS1_COLOR=`term_color fg_blue`;;
+  prod) PS1_COLOR=`term_color fg_red`;;
+  re7) PS1_COLOR=`term_color fg_green`;;
+  *) PS1_COLOR="`term_color fg_red`!!! TYPE_ENV not set !!!`echo``term_color fg_red``term_color bg_yellow`" ;;
+esac
 
-#load the configuration
-prof_last="$et/env/locals/profile/`whoami`@`hostname`.bash"
-
-if [ ! -f "$prof_last" ]; then
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  echo "The local last bash don't exists, creating it"
-  echo "file : [$prof_last]"
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-  {
-    echo '#!/usr/bin/env bash'
-    echo ''
-    echo "#bash for user `whoami`@`hostname`"
-    echo 'TYPE_ENV= #dev|prod|re7'
-    echo ''
-    echo '#__EOF__'
-  } >> $prof_last
-
-fi
-
-chmod u+x "$prof_last"
-
-. "$prof_last"
-unset prof_last
-
-if [ -z "${PS1_COLOR:-}" ]; then
-  case "${TYPE_ENV:-}" in
-    dev) PS1_COLOR=`term_color fg_blue`;;
-    prod) PS1_COLOR=`term_color fg_red`;;
-    re7) PS1_COLOR=`term_color fg_green`;;
-    *) PS1_COLOR="`term_color fg_red`!!! PS1_COLOR not set !!!`echo``term_color fg_red``term_color bg_yellow`" ;;
-  esac
-fi
-
-export TYPE_ENV="${TYPE_ENV:-}"
+export TYPE_ENV="${TYPE_ENV:-dont_know}"
 export SCREEN_SIZE="${SCREEN_SIZE:-150x51}"
-
-
 
 ###########################
 # Test program files (only if there is no config yet)
 ###########################
 
-#for cmd in $(sed 's/#.*$//g' "$et/env/conf/exe_exists.lst"); do
-#  which $cmd >/dev/null 2>&1 && continue
-#  echo "!!! $cmd is not found in this environment, you migth whant to add it !!!"
-#done
+if [[ "${TYPE_ENV:-}" == dont_know ]]; then
+  for cmd in $(sed 's/#.*$//g' "$conf/exe_exists.lst"); do
+    which $cmd >/dev/null 2>&1 && continue
+    echo "!!! $cmd is not found in this environment, you might whant to add it !!!"
+  done
+fi
 
+#bash variable sheet
+# ${toto#reg} delete the shortest match from the left
+# ${toto##reg} delete the longest match from the left
+# ${toto%reg} delete the shortest from the right
+# ${toto%%reg} delete the longest from the right
+# ${toto:-lala} $toto or "lala"
+# ${toto:=lala} $toto or toto="lala" ; $toto
+# ${toto:+lala} $toto and "lala" or ""
+# ${toto:?"lala"} $toto or echo "lala"
 
 
 #__EOF__
